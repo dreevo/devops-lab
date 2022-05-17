dc1b488fb4fc9e24364ae90cc468713b0a2a43bd
dreevo <sidedark85@gmail.com>
2022-05-17 16:30:28 +0100

    Initial Commit

---
+++ src/main/java/com/esprit/devopslab/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.esprit.devopslab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        boolean useless = false;
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/esprit/devopslab/DevopsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.devopslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
